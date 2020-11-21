e23c1a5da0556d682f0114a91a539e04440c8b59
DaVe-Smith-89 <73696370+DaVe-Smith-89@users.noreply.github.com>
2020-11-21 16:15:05 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet installing...
+sleep 1
+apt install toilet -y
+apt install cowsay -y
+apt install ruby -y
+gem install lolcat
+figlet CYBER | lolcat -a
+figlet worriors | lolcat -a
+pkg install nano -y
+clear
+bash banner.sh
