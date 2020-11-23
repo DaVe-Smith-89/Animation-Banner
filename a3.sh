3b88a3b5ab9a446efcd4ce5c58888b067177c86a
DaVe-Smith-89 <73696370+DaVe-Smith-89@users.noreply.github.com>
2020-11-23 11:06:44 +0530

    Update a3.sh

--- a3.sh
+++ a3.sh
@@ -10,19 +10,6 @@ lb='\e[1;36m'
 gr='\e[1;30m'
 wh='\e[1;37m'
 
-#colour code
-
-r='\e[1;31m'
-g='\e[1;32m'
-y='\e[1;33m'
-b='\e[1;34m'
-p='\e[1;35m'
-lb='\e[1;36m'
-gr='\e[1;30m'
-wh='\e[1;37m'
-
-echo -e $b"                                   By Dave Smith"
-echo -e $lb"                         owner of CYBER WORRIORS"
 echo
 while true; do
     read -p "Are you wan´t to make banner with animation( y /n )" yn
