19fc839b8c4bd644b39636785948ed939c92c086
DaVe-Smith-89 <73696370+DaVe-Smith-89@users.noreply.github.com>
2020-11-25 14:17:21 +0530

    Create bannergrenator.sh

---
+++ bannergrenator.sh
@@ -0,0 +1,83 @@
+clear
+pkg install figlet -y
+clear
+
+#coded by Dave Smith
+#2020 CYBER WORRIORS ®
+
+#colour code
+
+r='\e[1;31m'
+g='\e[1;32m'
+y='\e[1;33m'
+b='\e[1;34m'
+p='\e[1;35m'
+lb='\e[1;36m'
+gr='\e[1;30m'
+wh='\e[1;37m'
+
+#logo
+echo -e $g"
+██╗      ██████╗  ██████╗  ██████╗
+██║     ██╔═══██╗██╔════╝ ██╔═══██╗
+██║     ██║   ██║██║  ███╗██║   ██║
+██║     ██║   ██║██║   ██║██║   ██║
+███████╗╚██████╔╝╚██████╔╝╚██████╔╝
+╚══════╝ ╚═════╝  ╚═════╝  ╚═════╝"
+echo
+echo -e $wh"
+ ██████╗ ██████╗ ███████╗███╗   ██╗ █████╗ ████████╗ ██████╗ ██████╗
+██╔════╝ ██╔══██╗██╔════╝████╗  ██║██╔══██╗╚══██╔══╝██╔═══██╗██╔══██╗
+██║  ███╗██████╔╝█████╗  ██╔██╗ ██║███████║   ██║   ██║   ██║██████╔╝
+██║   ██║██╔══██╗██╔══╝  ██║╚██╗██║██╔══██║   ██║   ██║   ██║██╔══██╗
+╚██████╔╝██║  ██║███████╗██║ ╚████║██║  ██║   ██║   ╚██████╔╝██║  ██║
+ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝"
+echo
+echo -e $y"                                               Coded By Dave Smith"
+
+#calculation
+
+echo
+echo -e $g"                 What is your banner name:"$wh
+read a
+echo -e $lb
+
+#print
+
+figlet "$a"
+echo
+figlet -f big "$a"
+echo
+figlet -f smslant "$a"
+echo
+figlet -f smshadow "$a"
+echo
+figlet -f smscript "$a"
+echo
+figlet -f small "$a"
+echo
+figlet -f slant "$a"
+echo
+figlet -f shadow "$a"
+echo
+figlet -f mini "$a"
+echo
+figlet -f script "$a"
+echo
+figlet -f lean "$a"
+echo
+figlet -f ivrit "$a"
+echo
+figlet -f digital "$a"
+echo
+figlet -f bubble "$a"
+echo
+figlet -f block "$a"
+echo
+
+echo -e $g"All right reserved 2020 CYBER WORRIORS ®"
+echo
+figlet Done !!!
+echo -e $r"If you wan´t to make rename your banner type(bash logogrenator.sh)
+
+#Done
