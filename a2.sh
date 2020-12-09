c611a4686d80a3b6c30642fb628c1fd45abbda5f
DaVe-Smith-89 <73696370+DaVe-Smith-89@users.noreply.github.com>
2020-12-09 09:42:02 +0530

    Update a2.sh

--- a2.sh
+++ a2.sh
@@ -13,7 +13,7 @@ wh='\e[1;37m'
 
 echo
 while true; do
-    read -p "Are you wan´t to make banner with animation( y /n )" yn
+    read -p "Do you wan´t to make banner with animation( y /n )" yn
     case $yn in
         [Yy]* ) bash animationm.sh ; break;;
         [Nn]* ) bash evileyem.sh ;;
