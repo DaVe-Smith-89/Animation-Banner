27c3c5a970134dfb45a1b0a74a17ab9df2f9bb9d
Dave Smith <73696370+DaVe-Smith-89@users.noreply.github.com>
2021-06-19 15:39:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,3 @@
 clear
-apt update
-apt upgrade
-apt install figlet -y
-figlet installing...
-sleep 1
-apt install toilet -y
-apt install cowsay -y
-apt install ruby -y
-gem install lolcat
-figlet CYBER | lolcat -a
-figlet worriors | lolcat -a
-pkg install nano -y
+pkg install python -y
+python Adv-Banner.py
